d8b0aac4580185b1ca64946bd5c6faa207943a91
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-09-09 11:51:41 +0200

    feat: adding summary

--- {{ cookiecutter.__package_slug }}/main.go
+++ {{ cookiecutter.__package_slug }}/main.go
@@ -6,7 +6,8 @@ var date string
 var builtBy string
 var branch string
 var gitUrl string
+var summary string
 
 func main() {
-	println("ver", version, "commit", commit, "date", date, "builtBy", builtBy, "branch", branch, "gitUrl", gitUrl)
+	println("ver", version, "commit", commit, "date", date, "builtBy", builtBy, "branch", branch, "gitUrl", gitUrl, "summary", summary)
 }
