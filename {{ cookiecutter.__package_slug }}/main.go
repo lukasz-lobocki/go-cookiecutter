387796438e2cb4b2173d514947c1e0b48297f969
Lukasz Lobocki <125857607+lukasz-lobocki@users.noreply.github.com>
2023-09-09 12:10:30 +0200

    Adding more build variables (#1)

--- {{ cookiecutter.__package_slug }}/main.go
+++ {{ cookiecutter.__package_slug }}/main.go
@@ -4,7 +4,10 @@ var version string
 var commit string
 var date string
 var builtBy string
+var branch string
+var gitUrl string
+var semVer string
 
 func main() {
-	println("ver", version, "commit", commit, "date", date, "builtBy", builtBy)
+	println("ver", version, "commit", commit, "date", date, "builtBy", builtBy, "branch", branch, "gitUrl", gitUrl, "semVer", semVer)
 }
