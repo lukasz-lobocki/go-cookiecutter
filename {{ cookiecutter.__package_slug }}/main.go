e3008b97e8f66c5918c7afe7f3204cfa46cbf1be
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-09-09 12:07:12 +0200

    refactor: renaming to semVer

--- {{ cookiecutter.__package_slug }}/main.go
+++ {{ cookiecutter.__package_slug }}/main.go
@@ -6,8 +6,8 @@ var date string
 var builtBy string
 var branch string
 var gitUrl string
-var summary string
+var semVer string
 
 func main() {
-	println("ver", version, "commit", commit, "date", date, "builtBy", builtBy, "branch", branch, "gitUrl", gitUrl, "summary", summary)
+	println("ver", version, "commit", commit, "date", date, "builtBy", builtBy, "branch", branch, "gitUrl", gitUrl, "semVer", semVer)
 }
