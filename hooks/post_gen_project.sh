c58fd826880c794462bf218e23588dea15d6f612
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-09-10 16:52:42 +0200

    refactor(post_gen_project.sh): Parametrizing Github and user

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,6 +5,9 @@ IFS=$'\n\t'
 
 # chmod u+x __self__.sh
 
+GITURL='github.com'
+GITUSER='lukasz-lobocki'
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -21,7 +24,7 @@ git init
 echo -e "
 ${RED}>>> ${NC}Initiating go mod.
 "
-go mod init github.com/lukasz-lobocki/{{cookiecutter.__package_slug}}
+go mod init ${GITURL}/${GITUSER}/{{cookiecutter.__package_slug}}
 
 echo -e "
 ${RED}>>> ${NC}Adding and commiting ${GREEN}feat:${NC} all.
@@ -30,14 +33,14 @@ git add --all
 git commit -m "feat: Repo initiation."
 
 echo -e "
-${RED}>>> ${NC}Creating remote on GitHub.
+${RED}>>> ${NC}Creating remote on ${GITURL}.
 "
 gh repo create "{{cookiecutter.__package_slug}}" --public --description "{{cookiecutter.package_short_description}}"
 git branch --move --force main
-git remote add origin git@github.com:lukasz-lobocki/"{{cookiecutter.__package_slug}}"
+git remote add origin git@${GITURL}:${GITUSER}/"{{cookiecutter.__package_slug}}"
 
 echo -e "
-${RED}>>> ${NC}Adding tag and pushing to GitHub.
+${RED}>>> ${NC}Adding tag and pushing to ${GITURL}.
 "
 git tag --annotate v{{cookiecutter.package_version}} -m "Initiation."
 git push --set-upstream --tags origin main
