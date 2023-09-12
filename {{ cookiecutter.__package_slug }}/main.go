4ce4694a4bbb4d91548630cdf85c066391baeb7e
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-09-12 11:27:37 +0200

    chore

--- {{ cookiecutter.__package_slug }}/main.go
+++ {{ cookiecutter.__package_slug }}/main.go
@@ -1,16 +1,16 @@
 package main
 
 var (
-	semVer, commitHash  string
-        isGitDirty, isSnapshot string
-        goOs, goArch        string
-	gitUrl, builtBranch string
-	builtDate, builtBy  string
+	semVer, commitHash     string
+	isGitDirty, isSnapshot string
+	goOs, goArch           string
+	gitUrl, builtBranch    string
+	builtDate, builtBy     string
 )
 
 func main() {
-        println("build version:", semVer+"+"+goArch+"."+commitHash)
-        println()
+	println("build version:", semVer+"+"+goArch+"."+commitHash)
+	println()
 	println("semVer:", semVer, "commitHash:", commitHash)
 	println("isGitDirty:", isGitDirty, "isSnapshot:", isSnapshot)
 	println("goOs:", goOs, "goArch:", goArch)
