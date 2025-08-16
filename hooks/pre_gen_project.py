11bcec7a1730966838184ff0133246a82817700f
Lukasz Lobocki <lukasz.lobocki@googlemail.com>
2025-08-16 21:10:11 +0200

    fix: Update pre_gen_project

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,20 +1,11 @@
+import re
 import sys
 
-from pkg_resources import (
-    get_distribution,
-    packaging,
-)
+MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
-MIN_CC_VERSION = "2.0.0"
+module_name = "{{ cookiecutter.project_slug}}"
 
-# assert cookiecutter >= 2.0.0
-cc_version = packaging.version.parse(get_distribution("cookiecutter").version)
-min_version = packaging.version.parse(MIN_CC_VERSION)
-if cc_version < min_version:
-    print(
-        f"ERROR: please install cookiecutter >= {MIN_CC_VERSION} (current "
-        f"version is {cc_version}):\n"
-        f"\tpip install 'cookiecutter>=2', OR\n"
-        f"\tconda install 'cookiecutter>=2'"
-    )
+if not re.match(MODULE_REGEX, module_name):
+    print("ERROR: The project slug (%s) is not a valid Python module name. " "Please do not use a - and use _ instead" % module_name)
+    # Exit to cancel project
     sys.exit(1)
