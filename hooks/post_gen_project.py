ce44daa30ab9f0b0598ffd805f1ba45776d441ac
Lukasz Lobocki <lukasz.lobocki@googlemail.com>
2025-08-27 06:57:56 +0200

    chore

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,68 +18,185 @@ def remove(filepath):
 
 def replace_placeholder(file_path, placeholder, replacer):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             content = file.read()
 
         while re.search(placeholder, content):
-              color = replacer()
-              content = re.sub(placeholder, color, content, count=1)
+            color = replacer()
+            content = re.sub(placeholder, color, content, count=1)
 
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             file.write(content)
 
         print(f"\nPlaceholders {placeholder} in {file_path} replaced.")
-    
+
     except FileNotFoundError:
         print(f"\nFile {file_path} not found.")
     except Exception as e:
         print(f"\nAn error occurred: {str(e)}")
 
+
 def pick_random_css_color() -> str:
     css_colors = [
-        'black', 'silver', 'gray', 'white', 'maroon', 'red', 'purple', 'fuchsia',
-        'green', 'lime', 'olive', 'yellow', 'navy', 'blue', 'teal', 'aqua',
-        'orange', 'aliceblue', 'antiquewhite', 'aquamarine', 'azure', 'beige',
-        'bisque', 'blanchedalmond', 'blueviolet', 'brown', 'burlywood', 'cadetblue',
-        'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson',
-        'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen',
-        'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid',
-        'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray',
-        'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray',
-        'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'gainsboro',
-        'ghostwhite', 'gold', 'goldenrod', 'greenyellow', 'honeydew', 'hotpink',
-        'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush',
-        'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan',
-        'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightpink',
-        'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray',
-        'lightsteelblue', 'lightyellow', 'limegreen', 'linen', 'magenta',
-        'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple',
-        'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise',
-        'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin',
-        'navajowhite', 'oldlace', 'olivedrab', 'orangered', 'orchid', 'palegoldenrod',
-        'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff',
-        'peru', 'pink', 'plum', 'powderblue', 'rosybrown', 'royalblue', 'saddlebrown',
-        'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'skyblue',
-        'slateblue', 'slategray', 'snow', 'springgreen', 'steelblue', 'tan',
-        'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'whitesmoke',
-        'yellowgreen'
+        "black",
+        "silver",
+        "gray",
+        "white",
+        "maroon",
+        "red",
+        "purple",
+        "fuchsia",
+        "green",
+        "lime",
+        "olive",
+        "yellow",
+        "navy",
+        "blue",
+        "teal",
+        "aqua",
+        "orange",
+        "aliceblue",
+        "antiquewhite",
+        "aquamarine",
+        "azure",
+        "beige",
+        "bisque",
+        "blanchedalmond",
+        "blueviolet",
+        "brown",
+        "burlywood",
+        "cadetblue",
+        "chartreuse",
+        "chocolate",
+        "coral",
+        "cornflowerblue",
+        "cornsilk",
+        "crimson",
+        "cyan",
+        "darkblue",
+        "darkcyan",
+        "darkgoldenrod",
+        "darkgray",
+        "darkgreen",
+        "darkkhaki",
+        "darkmagenta",
+        "darkolivegreen",
+        "darkorange",
+        "darkorchid",
+        "darkred",
+        "darksalmon",
+        "darkseagreen",
+        "darkslateblue",
+        "darkslategray",
+        "darkturquoise",
+        "darkviolet",
+        "deeppink",
+        "deepskyblue",
+        "dimgray",
+        "dodgerblue",
+        "firebrick",
+        "floralwhite",
+        "forestgreen",
+        "gainsboro",
+        "ghostwhite",
+        "gold",
+        "goldenrod",
+        "greenyellow",
+        "honeydew",
+        "hotpink",
+        "indianred",
+        "indigo",
+        "ivory",
+        "khaki",
+        "lavender",
+        "lavenderblush",
+        "lawngreen",
+        "lemonchiffon",
+        "lightblue",
+        "lightcoral",
+        "lightcyan",
+        "lightgoldenrodyellow",
+        "lightgray",
+        "lightgreen",
+        "lightpink",
+        "lightsalmon",
+        "lightseagreen",
+        "lightskyblue",
+        "lightslategray",
+        "lightsteelblue",
+        "lightyellow",
+        "limegreen",
+        "linen",
+        "magenta",
+        "mediumaquamarine",
+        "mediumblue",
+        "mediumorchid",
+        "mediumpurple",
+        "mediumseagreen",
+        "mediumslateblue",
+        "mediumspringgreen",
+        "mediumturquoise",
+        "mediumvioletred",
+        "midnightblue",
+        "mintcream",
+        "mistyrose",
+        "moccasin",
+        "navajowhite",
+        "oldlace",
+        "olivedrab",
+        "orangered",
+        "orchid",
+        "palegoldenrod",
+        "palegreen",
+        "paleturquoise",
+        "palevioletred",
+        "papayawhip",
+        "peachpuff",
+        "peru",
+        "pink",
+        "plum",
+        "powderblue",
+        "rosybrown",
+        "royalblue",
+        "saddlebrown",
+        "salmon",
+        "sandybrown",
+        "seagreen",
+        "seashell",
+        "sienna",
+        "skyblue",
+        "slateblue",
+        "slategray",
+        "snow",
+        "springgreen",
+        "steelblue",
+        "tan",
+        "thistle",
+        "tomato",
+        "turquoise",
+        "violet",
+        "wheat",
+        "whitesmoke",
+        "yellowgreen",
     ]
     return random.choice(css_colors)
+
 
 def pick_random_diceware_pl() -> str:
     response = requests.get("https://github.com/MaciekTalaska/diceware-pl/raw/master/diceware-pl.txt")
     if response.status_code == 200:
-        lines = response.text.split('\n')
+        lines = response.text.split("\n")
         lines = [line.strip() for line in lines if line.strip()]
     else:
         print(f"Failed to load content from {response.url}. Status code: {response.status_code}")
-        return
+        return ""
         # Return a random line
     return random.choice(lines)
+
 
 ##############################################################################
 # README.md placeholders randomization
 ##############################################################################
 
-replace_placeholder("README.md", r'\[\[RND_CSS_COLOR\]\]', pick_random_css_color)
-replace_placeholder("README.md", r'\[\[RND_DICEWARE_PL\]\]', pick_random_diceware_pl)
+replace_placeholder("README.md", r"\[\[RND_CSS_COLOR\]\]", pick_random_css_color)
+replace_placeholder("README.md", r"\[\[RND_DICEWARE_PL\]\]", pick_random_diceware_pl)
--- {{ cookiecutter.__package_slug }}/main.go
+++ {{ cookiecutter.__package_slug }}/main.go
@@ -1,39 +1,38 @@
 package main
 
 var (
-	semVer      string
-	commitHash  string
-	isGitDirty  string
-	isSnapshot  string
-	goOs        string
-	goArch      string
-	gitUrl      string
-	builtBranch string
-	builtDate   string
+	semVer            string
+	commitHash        string
+	isGitDirty        string
+	isSnapshot        string
+	goOs              string
+	goArch            string
+	gitUrl            string
+	builtBranch       string
+	builtDate         string
+	semReleaseVersion string = semVer +
+		func(pre string, txt string) string {
+			if len(txt) > 0 {
+				return pre + txt
+			} else {
+				return ""
+			}
+		}("+", goArch) +
+		func(pre string, txt string) string {
+			if len(txt) > 0 {
+				return pre + txt
+			} else {
+				return ""
+			}
+		}(".", builtBranch) +
+		func(pre string, txt string) string {
+			if len(txt) > 0 {
+				return pre + txt
+			} else {
+				return ""
+			}
+		}(".", commitHash)
 )
-
-var semReleaseVersion string = semVer +
-	func(pre string, txt string) string {
-		if len(txt) > 0 {
-			return pre + txt
-		} else {
-			return ""
-		}
-	}("+", goArch) +
-	func(pre string, txt string) string {
-		if len(txt) > 0 {
-			return pre + txt
-		} else {
-			return ""
-		}
-	}(".", builtBranch) +
-	func(pre string, txt string) string {
-		if len(txt) > 0 {
-			return pre + txt
-		} else {
-			return ""
-		}
-	}(".", commitHash)
 
 func main() {
 	println("release version:", semReleaseVersion)
